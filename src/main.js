445df007d1351758ec7c440413785d34b0c0a165
man90 <man90@tuta.io>
2020-02-08 23:09:22 +0100

    Add Vuex

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import App from './components/App.vue'
 import router from './router'
 import VueLazyload from 'vue-lazyload'
 import './registerServiceWorker'
+import store from './store'
 
 Vue.use(VueLazyload)
 
 Vue.config.productionTip = false
 
 new Vue({
-	router,
-	render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
