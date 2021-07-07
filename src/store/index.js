4bfc9a425fa6798bf6b1db0c470f85726b1dc1db
man90 <man90@tuta.io>
2021-07-07 12:03:52 +0200

    Replace Vue.set method calls with setter calls

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,3 @@
-import Vue from 'vue'
 import Vuex from 'vuex'
 import { VuexLS } from './VuexLS'
 import { request } from '../api/request'
@@ -40,7 +39,7 @@ const store = Vuex.createStore({
 	},
 
 	plugins: [ VuexLS ],
-	
+
 	mutations: {
 		import(state, backup) {
 			Object.assign(state, backup)
@@ -51,60 +50,58 @@ const store = Vuex.createStore({
 		},
 
 		updateBoards(state, payload) {
-			for (let key in payload) {
-				Vue.set(state.boards, key, payload[key])
-			}
+			state.boards = { ...state.boards, ...payload }
 		},
 
 		updateThreadList(state, {boardName, payload, count, page}) {
-			if (state.threadLists[boardName] == undefined) {
-				Vue.set(state.threadLists, boardName, [])
+			if (state.threadLists[boardName] === undefined) {
+				state.threadLists[boardName] = []
 			}
 
 			for (let i = page * count; i < (page + 1) * count; i++) {
 				if (payload[i]) {
-					Vue.set(state.threadLists[boardName], i, payload[i])
+					state.threadLists[boardName][i] = payload[i]
 				}
 			}
 		},
 
 		updateThreads(state, payload) {
 			for (let thread of payload) {
-				Vue.set(state.threads, thread.id, thread)
+				state.threads[thread.id] = thread
 			}
 		},
-		
+
 		updatePostList(state, {threadId, payload, count, page}) {
-			if (state.postLists[threadId] == undefined) {
-				Vue.set(state.postLists, threadId, [])
+			if (state.postLists[threadId] === undefined) {
+				state.postLists[threadId] = []
 			}
 
 			if (page == 'tail') {
 				let totalPosts = state.threads[threadId].posts
 
 				for (let i = Math.max(totalPosts - count, 0), j = 0; i < totalPosts; i++, j++) {
-					Vue.set(state.postLists[threadId], i, payload[j])
+					state.postLists[threadId][i] = payload[j]
 				}
 			} else {
 				for (let i = page * count; i < (page + 1) * count; i++) {
-					Vue.set(state.postLists[threadId], i, payload[i])
+					state.postLists[threadId][i] = payload[i]
 				}
 			}
 		},
 
 		updatePosts(state, payload) {
 			for (let post of payload) {
-				Vue.set(state.posts, post.id, post)
+				state.posts[post.id] = post
 			}
 		},
 
 		updateFeed(state, {boardName, payload, count, page}) {
-			if (state.feedLists[boardName] == undefined) {
-				Vue.set(state.feedLists, boardName, [])
+			if (state.feedLists[boardName] === undefined) {
+				state.feedLists[boardName] = []
 			}
-			
+
 			for (let i = 0; i < count; i++) {
-				Vue.set(state.feedLists[boardName], page * count + i, payload[i])
+				state.feedLists[boardName][page * count + i] = payload[i]
 			}
 		},
 
@@ -121,7 +118,7 @@ const store = Vuex.createStore({
 		},
 
 		clearSelected(state) {
-			clearList(state, 'selectedPostsList')			
+			clearList(state, 'selectedPostsList')
 		},
 
 		setTrustedPostCount(state, payload) {
@@ -203,19 +200,19 @@ request.init(store.state.APIServer, (message) => { // API response handlers
 
 		case 'threads':
 			store.commit('updateThreadList', {
-				boardName: message.what.boardName, 
-				payload: message.data.map(thread => thread.id), 
-				count: message.what.count, 
+				boardName: message.what.boardName,
+				payload: message.data.map(thread => thread.id),
+				count: message.what.count,
 				page: message.what.page
 			})
 
 			store.commit('updateThreads', message.data)
-			
+
 			for (let thread of message.data) {
 				store.commit('updatePostList', {
-					threadId: thread.id, 
-					payload: [thread.head.id], 
-					count: 1, 
+					threadId: thread.id,
+					payload: [thread.head.id],
+					count: 1,
 					page: 0
 				})
 
@@ -226,11 +223,11 @@ request.init(store.state.APIServer, (message) => { // API response handlers
 
 		case 'thread':
 			store.commit('updateThreads', [message.data])
-			
+
 			store.commit('updatePostList', {
-				threadId: message.data.id, 
-				payload: [message.data.head.id], 
-				count: 1, 
+				threadId: message.data.id,
+				payload: [message.data.head.id],
+				count: 1,
 				page: 0
 			})
 
@@ -239,20 +236,20 @@ request.init(store.state.APIServer, (message) => { // API response handlers
 			break
 
 		case 'posts':
-			if (message.what.boardName) { 
+			if (message.what.boardName) {
 				// Feed
 				store.commit('updateFeed', {
-					boardName: message.what.boardName, 
-					payload: message.data.map(post => post.id), 
-					count: message.what.count, 
+					boardName: message.what.boardName,
+					payload: message.data.map(post => post.id),
+					count: message.what.count,
 					page: message.what.page
 				})
-			} else { 
+			} else {
 				// Thread
 				store.commit('updatePostList', {
-					threadId: message.what.threadId, 
-					payload: message.data.map(post => post.id), 
-					count: message.what.count, 
+					threadId: message.what.threadId,
+					payload: message.data.map(post => post.id),
+					count: message.what.count,
 					page: message.what.page
 				})
 			}
