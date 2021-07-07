edfd50bca360588823d5b37a7fa80a5d52052e23
man90 <man90@tuta.io>
2021-07-07 11:21:33 +0200

    Remove libraries that don't work with Vue3

--- src/main.js
+++ src/main.js
@@ -1,15 +1,12 @@
 import Vue, { createApp } from 'vue';
-import VueBus from 'vue-bus'
 import App from './App.vue'
 import router from './router'
-import VueLazyload from 'vue-lazyload'
-import './registerServiceWorker'
 import store from './store'
-import Vue2TouchEvents from 'vue2-touch-events'
+import './registerServiceWorker'
 
-Vue.use(VueLazyload)
-Vue.use(VueBus)
-Vue.use(Vue2TouchEvents)
+// Vue.use(VueLazyload)
+// Vue.use(VueBus)
+// Vue.use(Vue2TouchEvents)
 
 window.vm = createApp(App)
 	.use(router)
