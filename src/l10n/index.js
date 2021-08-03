155541a3e4384a22ac8a46e2b6c7783ca376877a
man90 <man90@tuta.io>
2021-08-03 11:33:59 +0200

    Add Russian translation

--- src/l10n/index.js
+++ src/l10n/index.js
@@ -1,8 +1,10 @@
 import { createI18n } from "vue-i18n"
 import en from "./en"
+import ru, { ruPluralisationRules } from "./ru"
 
 export default createI18n({
 	locale: "en",
 	fallbackLocale: "en",
-	messages: { en },
+	messages: { en, ru },
+	pluralizationRules: { ru: ruPluralisationRules }
 })
---
+++ src/l10n/ru.js
@@ -0,0 +1,85 @@
+export default {
+	ok: "Да",
+	cancel: "Отмена",
+
+	settingsModal: {
+		header: "Настройки",
+		lightTheme: "Светлая тема",
+		compactBoardMenu: "Компактное меню досок",
+		repliesOnBoardPage: "Ответов на странице доски",
+	},
+
+	searchModal: {
+		header: "Поиск",
+		query: "Запрос",
+		boardName: "Название доски",
+		threadNumber: "Номер треда",
+		after: "После",
+		before: "До",
+	},
+
+	formModal: {
+		threadHeader: "Новый тред на доске /{boardName}",
+		postHeader: "Ответ в тред #{threadNumber} на доске /{boardName}",
+	},
+
+	captchaModal: {
+		header: "Проверка",
+		code: "Капча",
+	},
+
+	deletePostModal: {
+		header: "Удалить {count} выбранный пост? | Удалить {count} выбранных поста? | Удалить {count} выбранных постов?",
+		warning1: "Удаление не удастся, если у Вас недостаточно прав.",
+		warning2: "Удаление главного поста удаляет весь тред.",
+	},
+
+	unsafeLinkModal: {
+		header: "Вы точно хотите перейти по ссылке?",
+		warning: "Она может быть небезопасна.",
+	},
+
+	pageHeader: {
+		bookmarksTitle: "Закладки",
+		bookmarksDescription: "Посты отмеченные звездочкой",
+		boardTitle: "/{boardName}/ — {boardTitle}",
+		feedTitle: "Лента /{boardName}/ — {boardTitle}",
+		boardDescription: "{boardDescription}",
+	},
+
+	post: {
+		recently: "только что",
+		yesterday: "вчера",
+		minutesAgo: "{count} минуту назад | {count} минуты назад | {count} минут назад",
+		hoursAgo: "{count} час назад | {count} часа назад | {count} часов назад",
+		daysAgo: "{count} день назад | {count} дня назад | {count} дней назад",
+		omittedCount: "Пропущено постов: {count}",
+	},
+
+	postMenu: {
+		show: "Показать",
+		hide: "Скрыть",
+		unbookmark: "Убрать из закладок",
+		bookmark: "Добавить в закладки",
+		undelete: "Не удалять",
+		delete: "Удалить",
+	},
+}
+
+export function ruPluralisationRules(choice, choicesLength) {
+	if (1 === choicesLength) return 0
+
+	let t = choice % 100
+	if (choice > 19) t %= 10
+
+	switch (t) {
+		case 1:
+			return 0
+		case 2:
+		case 3:
+		case 4:
+			return 1
+		default:
+			return 2
+	}
+}
