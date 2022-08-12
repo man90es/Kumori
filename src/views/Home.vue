ced7c5bdae9bff599260163b2e21ba744fb98ef0
man90 <man90@tuta.io>
2022-08-12 18:18:30 +0200

    Create authorisation form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,81 @@
 <template>
 	<div id="home">
-		<div :style="backgroundStyle"></div>
-		<img src="logo.svg">
+		<div id="background-layer" :style="backgroundStyle" />
+		<img v-if="0 === loginPhase" src="logo.svg" @click="() => ++loginPhase" />
+		<form v-else-if="1 === loginPhase">
+			<input
+				:disabled="formValues.invite.length"
+				placeholder="Username"
+				type="text"
+				v-model="formValues.username"
+			/>
+			or
+			<input
+				:disabled="formValues.username.length"
+				placeholder="Invite code"
+				type="text"
+				v-model="formValues.invite"
+			/>
+			<button type="button" @click="submit">Submit</button>
+		</form>
+		<form v-else-if="2 === loginPhase">
+			<input placeholder="Username" type="text" v-model="formValues.username" />
+			<input type="text" placeholder="Email" v-model="formValues.email" />
+			<input type="password" placeholder="Password" v-model="formValues.password" />
+			<button type="button" @click="register">Create account</button>
+			<button type="button" @click="() => (loginPhase = 1)">Back</button>
+		</form>
+		<form v-else-if="3 === loginPhase">
+			<input type="password" placeholder="Password" v-model="formValues.password" />
+			<button type="button" @click="login">Log in</button>
+			<button type="button" @click="() => (loginPhase = 1)">Back</button>
+		</form>
 		<ul>
 			<li v-for="boardName in $store.state.boardList" :key="boardName">
-				<router-link :to="{name: 'board', params: {boardName}}">/{{boardName}}</router-link>
+				<router-link :to="{ name: 'board', params: { boardName } }"> /{{ boardName }} </router-link>
 			</li>
 		</ul>
 	</div>
 </template>
 
 <script setup>
-	import { computed } from "vue"
+	import { computed, ref, reactive } from "vue"
+
+	const loginPhase = ref(0)
+	const formValues = reactive({
+		email: "",
+		invite: "",
+		password: "",
+		username: "",
+	})
+
+	function submit() {
+		loginPhase.value = formValues.invite ? 2 : formValues.username ? 3 : 1
+	}
+
+	function register() {
+		const data = [formValues.invite, formValues.username, formValues.email, formValues.password]
+
+		if (4 > data.filter(Boolean).length) {
+			return
+		}
+
+		console.log(...data)
+	}
+
+	function login() {
+		const data = [formValues.username, formValues.password]
+
+		if (2 > data.filter(Boolean).length) {
+			return
+		}
+
+		console.log(...data)
+	}
 
 	const backgroundStyle = computed(() => {
-		return `background-image:url(https://picsum.photos/${Math.round(window.screen.width / 2)}/${Math.round(window.screen.height / 2)})`
+		const bgSize = [window.screen.width, window.screen.height].map((s) => Math.round(s / 2))
+		return `background-image:url(https://picsum.photos/${bgSize.join("/")})`
 	})
 </script>
 
@@ -26,27 +87,28 @@
 		justify-content: center;
 		align-items: center;
 		background-color: #000;
+	}
 
-		& > :first-child {
-			position: absolute;
-			top: 0;
-			right: 0;
-			bottom: 0;
-			left: 0;
-			background-position: center;
-			background-size: cover;
-			filter: url("#sharpBlur");
-			opacity: .4;
-		}
+	#background-layer {
+		background-position: center;
+		background-size: cover;
+		bottom: 0;
+		filter: url("#sharpBlur");
+		left: 0;
+		opacity: 0.4;
+		position: absolute;
+		right: 0;
+		top: 0;
 	}
 
 	img {
-		z-index: 1;
-		height: 15rem;
-		border-radius: 50%;
-		padding: 1.5rem 1rem;
 		background-color: #fff1;
-		transition: padding .2s, background-color .2s;
+		border-radius: 50%;
+		cursor: pointer;
+		height: 15rem;
+		padding: 1.5rem 1rem;
+		transition: padding 0.2s, background-color 0.2s;
+		z-index: 1;
 
 		&:hover {
 			padding: 2.5rem 2rem;
@@ -72,13 +134,44 @@
 	}
 
 	a {
-		padding: .25em 0;
 		color: #a8afbc;
 		display: block;
-		transition: background-color .2s;
+		padding: 0.25em 0;
+		transition: background-color 0.2s;
 
 		&:hover {
 			background-color: #fff1;
+		}
+	}
+
+	form {
+		align-items: center;
+		background-color: #fff2;
+		border-radius: 50%;
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
+		height: 20rem;
+		justify-content: center;
+		width: 20.75rem;
+		z-index: 1;
+
+		& > input {
+			border: 0;
+			padding: 0.5em;
+			text-align: center;
+			width: 20em;
+		}
+
+		& > button {
+			background-color: var(--card-secondary-color);
+			padding: 0.5em 2em;
+			width: 10em;
+			color: var(--text-color);
+
+			&:not(:first-of-type) {
+				background-color: var(--card-color);
+			}
 		}
 	}
 </style>
