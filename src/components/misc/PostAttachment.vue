5e3f607419f30724e0f59d04a092db40002ce46f
man90 <man90@tuta.io>
2024-03-07 18:21:39 +0100

    Rewrite PostAttachment component in TS

--- src/components/misc/PostAttachment.vue
+++ src/components/misc/PostAttachment.vue
@@ -1,39 +1,47 @@
 <template>
-	<div>
-		<img :class="{ nsfw: isNSFW }" :height="API.thumb.height" :src="`${API.thumb.path}${file.hash}.${API.thumb.format}`" :width="API.thumb.width" @click="clickHandler" class="thumbnail" loading="lazy" />
-		<img class="video-marker" src="@/assets/icons/play_circle.svg" v-if="isVideo" />
+	<div :class="$style.root">
+		<img :class="{ [$style.thumbnail]: true, [$style.blurred]: isNSFW }" :src="thumbSrc" :height="API.thumb.height" :width="API.thumb.width" @click="handleClick" loading="lazy" />
+		<img :class="$style.videoMarker" src="@/assets/icons/play_circle.svg" v-if="isVideo" />
 	</div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 	import { computed, inject } from "vue"
 
 	const API = inject("API")
-	const props = defineProps({ file: Object })
 
+	const props = defineProps<{
+		file: {
+			hash: string,
+			mime: string,
+			modifiers?: string[],
+		}
+	}>()
+
+	const thumbSrc = computed(() => `${API.thumb.path}${props.file.hash}.${API.thumb.format}`)
 	const isNSFW = computed(() => props.file.modifiers?.includes("NSFW"))
 	const isVideo = computed(() => "video" == props.file.mime?.split("/")[0])
 
-	function clickHandler() {
+	function handleClick() {
 		window.emitter.emit("post-attachment-preview-click", props.file)
 	}
 </script>
 
-<style scoped lang="scss">
-	div {
+<style module>
+	.root {
 		position: relative;
 	}
 
 	.thumbnail {
 		background-color: var(--background-color);
 		cursor: pointer;
-
-		&.nsfw {
-			filter: url("#sharpBlur");
-		}
 	}
 
-	.video-marker {
+	.blurred {
+		filter: url("#sharpBlur");
+	}
+
+	.videoMarker {
 		filter: invert(1);
 		height: 5em;
 		left: calc(50% - 2.5em);
