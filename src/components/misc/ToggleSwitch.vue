110b39740c973783a1c1d87f937e703060d78bdd
man90 <man90@tuta.io>
2024-03-07 18:25:30 +0100

    Rewrite ToggleSwitch component in TS

--- src/components/misc/ToggleSwitch.vue
+++ src/components/misc/ToggleSwitch.vue
@@ -2,10 +2,10 @@
 	<img class="icon" :src="require(`@/assets/icons/toggle_${on ? 'on' : 'off'}.svg`)" @click="toggle" />
 </template>
 
-<script setup>
+<script setup lang="ts">
 	import { ref } from "vue"
 
-	const props = defineProps({ modelValue: Boolean })
+	const props = defineProps<{ modelValue: boolean }>()
 	const emit = defineEmits(["update:modelValue"])
 
 	const on = ref(props.modelValue)
